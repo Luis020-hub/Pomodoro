81a7d1d4630a7174ab6d221f5417e300f25ab05d
Luis020-hub <guhmonteiro10@hotmail.com>
2023-05-16 23:21:22 -0300

    16/05/2023
    - Colocando só 1 observer para o contador.
    - Alterando entre tempo de trabalho e tempo de descanso.
    - Alterando o projeto para versões mais compatíveis.

--- lib/components/counter.dart
+++ lib/components/counter.dart
@@ -11,62 +11,64 @@ class Counter extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = Provider.of<PomodoroStore>(context);
 
-    return Container(
-      color: store.working() ? Colors.red : Colors.green,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            store.working() ? 'Time to Work' : 'Time to Rest',
-            style: const TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            '${store.minutes.toString().padLeft(2, '0')}:${store.seconds.toString().padLeft(2, '0')}',
-            style: const TextStyle(
-              fontSize: 120,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Observer(
-            builder: (_) => Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (store.started == false)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: CounterButton(
-                      text: 'Start',
-                      icon: Icons.play_arrow,
-                      click: store.start,
-                    ),
-                  ),
-                if (store.started == true)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: CounterButton(
-                      text: 'Stop',
-                      icon: Icons.stop,
-                      click: store.stop,
-                    ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: CounterButton(
-                    text: 'Restart',
-                    icon: Icons.refresh,
-                    click: store.restart,
-                  ),
+    return Observer(
+      builder: (_) {
+        return Container(
+          color: store.working() ? Colors.red : Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                store.working() ? 'Time to Work' : 'Time to Rest',
+                style: const TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                '${store.minutes.toString().padLeft(2, '0')}:${store.seconds.toString().padLeft(2, '0')}',
+                style: const TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (store.started == false)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: CounterButton(
+                        text: 'Start',
+                        icon: Icons.play_arrow,
+                        click: store.start,
+                      ),
+                    ),
+                  if (store.started == true)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: CounterButton(
+                        text: 'Stop',
+                        icon: Icons.stop,
+                        click: store.stop,
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: CounterButton(
+                      text: 'Restart',
+                      icon: Icons.refresh,
+                      click: store.restart,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: constant_identifier_names, library_private_types_in_public_api
 
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
@@ -27,19 +29,32 @@ abstract class _PomodoroStore with Store {
   @observable
   BreakType breakType = BreakType.REST;
 
+  Timer? counter;
+
   @action
   void start() {
     started = true;
+    counter = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (minutes == 0 && seconds == 0) {
+        _swapeBreakTime();
+      } else if (seconds == 0) {
+        seconds = 59;
+        minutes--;
+      } else {
+        seconds--;
+      }
+    });
   }
 
   @action
   void stop() {
     started = false;
+    counter?.cancel();
   }
 
   @action
   void restart() {
-    started = false;
+    stop();
   }
 
   @action
@@ -68,5 +83,16 @@ abstract class _PomodoroStore with Store {
 
   bool resting() {
     return breakType == BreakType.REST;
+  }
+
+  void _swapeBreakTime() {
+    if (working()) {
+      breakType = BreakType.REST;
+      minutes = restTime;
+    } else {
+      breakType = BreakType.WORK;
+      minutes = workTime;
+    }
+    seconds = 0;
   }
 }
